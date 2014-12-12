dc3f6f1a5b1c9cf6f564185dc0afef308733261c
Victor Fernando de Oliveira Sampaio <victorfsampaio@gmail.com>
2014-12-12 17:01:47 -0300

    Project_JavaWeb #1.1
    Refactoring code: 'most elegant' mode

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -2,29 +2,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 /**
- * 
- */
-
-/**
  * @author VictorSampaio
  *
  */
-/*
 public class ConnectionFactory {
 	
-	
-	public Connection getConnection() {
+		public Connection getConnection() {
 		try {
-			System.out.println("-- Entrou no try --");
+			System.out.println("--- Try ---");
 			return DriverManager.getConnection(
 					"jdbc:mysql://localhost/test","root","vertrigo");
 			
 		} catch (Exception e) {
-			System.out.println("-- Entrou no catch - Exception --");
+			System.out.println("--- Catch - Exception ---");
 			System.out.println("ERROR: " + e.getMessage());
 			e.getMessage();
 			throw new RuntimeException(e);
 		}		
 	}
 }
-*/
--- src/TesteInsere.java
+++ src/TesteInsere.java
@@ -4,29 +4,53 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
- * 
- */
-
-/**
  * @author VictorSampaio
  *
  */
+
 public class TesteInsere {
 
-	/**
-	 * @param args
-	 * @throws SQLException 
-	 */
-	public static void main(String[] args) throws SQLException {
-		
-		
-		Connection conexao = DriverManager.getConnection(
-				"jdbc:mysql://localhost/test","root","vertrigo");
-				System.out.println("Conectado!");
-				conexao.close();
+			/**
+			 * @param args
+			 * @throws SQLException 
+			 */
+			public static void main(String[] args) throws SQLException {
+				
+				Connection con = null;
+				try {
+				con = new ConnectionFactory().getConnection();
+				System.out.println("--------------");
+				System.out.println("Connection: Open!");
+				System.out.println("--------------");
+				
+				String sql = "insert into contato" + " (nome,email,endereco)" + " values(?,?,?)";
+				
+				PreparedStatement stmt = con.prepareStatement(sql);	
+				
+				//Values
+				stmt.setString(1,"Tiago");
+				stmt.setString(2,"tico@email.com");
+				stmt.setString(3,"rua tico 123");
+				
+				stmt.execute();
+				System.out.println("--//--//--//--//--");
+				System.out.println("Executing 'sql' !");
+				System.out.println("--//--//--//--//--");
+			
+				stmt.close();
+				System.out.println("-- Gravado --");
+														
+				} catch (Exception e){
+					System.out.println("ERROR: ");
+					System.out.println(e);
+				} finally {
+					con.close();
+					System.out.println("--//--//--//--//--");
+					System.out.println("Connection: Close!");
+					System.out.println("--//--//--//--//--");
+				}
+			}
 	
-		
-		
 	}
 
-}
+
